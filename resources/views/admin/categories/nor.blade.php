6505538254fd4cae7cd76474666c0c8fb1231d65
An0413 <aleqsanyan.an@bk.ru>
2023-05-03 14:44:23 +0400

    nor blade ej

---
+++ resources/views/admin/categories/nor.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Nor fayl</title>
+    <style>
+        h1{
+            text-align:center;
+            color: lightskyblue;
+            font-size: 25px;
+        }
+        ul{
+            background-color: lightskyblue;
+            color: black;
+            list-style-type: none;
+        }
+    </style>
+</head>
+<body>
+<h1>Git-ի ՀՐԱՄԱՆՆԵՐԸ</h1>
+<ul>
+    <li>git init</li>
+    <li>git add .</li>
+    <li>git status</li>
+    <li>git commit -m "text"</li>
+    <li>git push -u origin main</li>
+    <li></li>
+    <li></li>
+</ul>
+</body>
+</html>
